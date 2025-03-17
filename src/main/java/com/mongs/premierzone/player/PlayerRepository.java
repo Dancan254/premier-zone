b53f7de1a244c1f326b51f821f12a53fd172a348
Dancan254 <dancanian25@gmail.com>
2025-03-17 15:22:52 +0300

    Add PlayerRepository interface for player data access

---
+++ src/main/java/com/mongs/premierzone/player/PlayerRepository.java
@@ -0,0 +1,14 @@
+package com.mongs.premierzone.player;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, String> {
+    void deleteByName(String name);
+    Optional<Player> findByName(String name);
+    List<Player> findByNation(String nation);
+    List<Player> findByPosition(String position);
+}
