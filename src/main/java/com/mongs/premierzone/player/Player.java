3a7f82768d59375c9f9aec9012a5770d4c6ecb6a
Dancan254 <dancanian25@gmail.com>
2025-03-17 16:37:08 +0300

    Rename player_stats table to prem_stats and add additional player attributes

--- src/main/java/com/mongs/premierzone/player/Player.java
+++ src/main/java/com/mongs/premierzone/player/Player.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name ="player_stats")
+@Table(name ="prem_stats")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,19 +14,33 @@ public class Player {
     @Id
     @Column(name = "name")
     private String name;
+    @Column(name = "nation")
     private String nation;
+    @Column(name = "pos")
     private String pos;
+    @Column(name = "age")
     private Integer age;
+    @Column(name = "mp")
     private Integer mp;
+    @Column(name = "starts")
     private Integer starts;
+    @Column(name = "min")
     private Double min;
+    @Column(name = "gls")
     private Double gls;
+    @Column(name = "ast")
     private Double ast;
+    @Column(name = "pk")
     private Double pk;
+    @Column(name = "crdy")
     private Double crdy;
+    @Column(name = "crdr")
     private Double crdr;
+    @Column(name = "xg")
     private Double xg;
+    @Column(name = "xag")
     private Double xag;
+    @Column(name = "team")
     private String team;
 
 
